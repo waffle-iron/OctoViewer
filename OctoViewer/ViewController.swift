45addd8163e404e0e5429f892265178216ae0016
heshamsalman <hesham.salman@viewthespace.com>
2017-05-20 16:44:46 -0400

    Initial Commit

---
+++ OctoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OctoViewer
+//
+//  Created by Hesham Salman on 5/20/17.
+//  Copyright © 2017 Hesham Salman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
