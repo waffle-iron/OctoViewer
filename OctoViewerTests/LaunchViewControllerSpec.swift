10c77ec2e0cb3ed3015d8a9200e8eec0fde6f5ea
heshamsalman <hesham.salman@viewthespace.com>
2017-05-21 11:46:15 -0400

    Spec

--- OctoViewerTests/LaunchViewControllerSpec.swift
+++ OctoViewerTests/LaunchViewControllerSpec.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Hesham Salman. All rights reserved.
 //
 
+import Foundation
 import Nimble
 import Moya
 import Quick
@@ -15,11 +16,10 @@ import ReusableViews
 class LaunchViewControllerSpec: QuickSpec {
   override func spec() {
     var controller: LaunchViewController!
-
     beforeEach {
       controller = UIStoryboard(name: "Splash", bundle: OctoViewer.bundle).instantiateViewControllerOfType(LaunchViewController.self)
       controller.loadView()
-      controller.provider = MoyaProvider(stubClosure: MoyaProvider.immediatelyStub)
+      controller.viewModel = LaunchViewModel(provider:  ReactiveSwiftMoyaProvider(stubClosure: MoyaProvider.immediatelyStub))
     }
 
     describe("networking") {
@@ -32,5 +32,32 @@ class LaunchViewControllerSpec: QuickSpec {
         expect(controller.zenLabel.text).toNot(beNil())
       }
     }
+
+    describe("navigation bar visibility") {
+      beforeEach {
+        _ = UINavigationController(rootViewController: controller)
+      }
+      
+      context("when the view is visible") {
+        beforeEach {
+          controller.viewWillAppear(false)
+        }
+
+        it("has a hidden navigation bar") {
+          expect(controller.navigationController?.isNavigationBarHidden).to(beTrue())
+        }
+      }
+
+      context("when the view is not visible") {
+        beforeEach {
+          controller.viewWillDisappear(false)
+        }
+
+        it("has a visible navigation bar") {
+          expect(controller.navigationController?.isNavigationBarHidden).to(beFalse())
+        }
+      }
+    }
+
   }
 }
